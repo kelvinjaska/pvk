3f1f5fe4e73a9310c18c69ad9282835e2a50bf83
kelvinjaska <kelvin.jaska@gmail.com>
2019-01-15 11:02:33 +0200

    index php fail

--- menu/index.php
+++ menu/index.php
@@ -1,7 +1,5 @@
 <?php
-/**
- * Template name: main
- */
+echo '
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -158,5 +156,5 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 </body>
-</html>
+</html>';
 ?>

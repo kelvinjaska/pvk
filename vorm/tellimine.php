c7f7d4b5321433b6bbe45d009e895269f4c481f5
kelvinjaska <kelvin.jaska@gmail.com>
2019-01-21 13:28:33 +0200

    Php lisamine

---
+++ vorm/tellimine.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kechsm
+ * Date: 2019-01-21
+ * Time: 13:20
+ */
+?>
+<h1>Tellimine</h1>
+<?php
+//lisab vormist saadud andmed muutujasse
+$toode1 = $_GET['t1'];
+
+echo 'Toode 1: '.$toode1.'tk';
+?>
